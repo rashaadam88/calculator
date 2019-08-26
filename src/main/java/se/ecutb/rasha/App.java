03ef4ff0ec4203e0d2beb7a31799d9393c6ac7f3
rashaadam88 <rashaadam88@yahoo.com>
2019-08-26 11:31:27 +0200

    First Commit

---
+++ src/main/java/se/ecutb/rasha/App.java
@@ -0,0 +1,28 @@
+package se.ecutb.rasha;
+
+
+public class App 
+{
+    public static void main(String[] args) {
+        int num1 = 45;
+        int num2 = 11;
+        int sum = num1 + num2;
+
+        int num3 = 12;
+        int num4 = 4;
+        int multiplication = num3 * num4;
+
+        int num5 = 24;
+        int num6 = 6;
+        int division = num5 / num6;
+
+        int num7 = 55;
+        int num8 = 12;
+        int subtraction = num7 - num8;
+
+        System.out.println("sum = " + sum);
+        System.out.println("multiplication = " + multiplication);
+        System.out.println("division =" + division );
+        System.out.println("subtraction = "+ subtraction);
+    }
+}
